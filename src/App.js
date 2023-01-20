09a60f42de6f004000708368c68af496a8f4588f
Pedro <pedrohalmeidamendonca@gmail.com>
2023-01-20 14:39:50 -0300

    iniciando o projeto. VAMOS COM TUDO :rocket:

--- src/App.js
+++ src/App.js
@@ -1,10 +1,3 @@
-import React from 'react';
-import './App.css';
-
-function App() {
-  return (
-    <span>Hello, App!</span>
-  );
+export default function App() {
+  return <div>Hello, App!</div>;
 }
-
-export default App;
